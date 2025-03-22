627976eb993a9c6f894b006ba373e5bde1adc56b
Danilo Machuca de Souza <danilo.machuca111@gmail.com>
2025-03-22 02:19:41 -0300

    inital commit

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require("gulp");
+const imagemin = require("gulp-imagemin");
+const sass = require("gulp-sass")(require("sass"));
+const uglify = require("gulp-uglify");
+
+function imagens() {
+  return gulp
+    .src("src/imgages/**/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("dist/images"));
+}
+
+function css() {
+  return gulp
+    .src("src/styles/*.scss")
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(gulp.dest("dist/styles/css"));
+}
+
+function js() {
+  return gulp
+    .src("src/scripts/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("dist/scripts/js"));
+}
+
+exports.default = gulp.parallel(imagens, css, js);
+
+exports.watch = function () {
+  gulp.watch("src/styles/*.scss", css);
+  gulp.watch("src/scripts/*.js", js);
+};
