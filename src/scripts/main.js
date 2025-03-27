0c86b460d3428b3cfb14d1dd0a97292119fdcdb4
Danilo Machuca de Souza <danilo.machuca111@gmail.com>
2025-03-27 11:42:26 -0300

    fix: story selection + add: section - filmes anteriores

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,6 +7,12 @@ document.addEventListener("DOMContentLoaded", function () {
       const aba = document.querySelector(`[data-story-id=${abaAlvo}]`);
       hideStorys();
       aba.classList.add("story-select--is-active");
+      removeActive();
+      if (buttons[i].classList.contains("button--hero")) {
+        buttons[i].classList.add("button--hero--is-active");
+      } else {
+        buttons[i].classList.add("button--dark--is-active");
+      }
     });
   }
 });
@@ -15,5 +21,13 @@ function hideStorys() {
   const storys = document.querySelectorAll("[data-story-id]");
   for (let i = 0; i < storys.length; i++) {
     storys[i].classList.remove("story-select--is-active");
+  }
+}
+
+function removeActive() {
+  const buttons = document.querySelectorAll("[data-story-button]");
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].classList.remove("button--hero--is-active");
+    buttons[i].classList.remove("button--dark--is-active");
   }
 }
